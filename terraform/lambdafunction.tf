b99f955f17ab39b329537ce7e742714be35ee18d
C3drik <olufemiphilips@outlook.com>
2023-03-14 23:26:26 +0100

    api

--- terraform/lambdafunction.tf
+++ terraform/lambdafunction.tf
@@ -99,7 +99,7 @@ resource "aws_api_gateway_resource" "counter" {
 
 resource "aws_api_gateway_method" "post" {
     rest_api_id = aws_api_gateway_rest_api.counter-api-gateway.id
-    resource_id = aws_api_gateway_resource.counter
+    resource_id = aws_api_gateway_resource.counter.id
     http_method = "POST"
     authorization = "NONE"
     api_key_required = false
